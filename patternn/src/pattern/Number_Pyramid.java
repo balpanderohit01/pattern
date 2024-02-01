35496275a0b94a7679a0fbef83fb0d52fd82e36f
balpanderohit01 <balpanderohit01.com>
2024-02-01 18:39:10 +0530

    number pyramid

---
+++ patternn/src/pattern/Number_Pyramid.java
@@ -0,0 +1,34 @@
+package pattern;
+import java.util.*;
+//Java Program to print pattern
+//Number-increasing pyramid 
+public class Number_Pyramid {
+	// Function to demonstrate pattern
+		public static void printPattern(int n)
+		{
+			int i, j;
+			// outer loop to handle number of rows
+			for (i = 1; i <= n; i++) {
+				// inner loop to handle number of columns
+				for (j = 1; j <= i; j++) {
+					// printing column values upto the row
+					// value.
+					System.out.print(j + " ");
+				}
+
+				// print new line for each row
+				System.out.println();
+			}
+		}
+
+		// Driver Function
+		public static void main(String args[])
+		{
+			int n = 6;
+			printPattern(n);
+		}
+}
+
+
+	
+
